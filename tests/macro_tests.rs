de528616ab221329542650fd3c5ab34ffd88fcec
Sam Johnson <sam@durosoft.com>
2024-06-05 12:39:38 -0400

    add amt! macro

--- tests/macro_tests.rs
+++ tests/macro_tests.rs
@@ -15,3 +15,15 @@ fn test_amt_usd_checked() {
     let amount: Amount<USD, Checked> = amt_checked!(USD, "$3.24");
     assert_eq!(format!("{}", amount), "$3.24");
 }
+
+#[test]
+fn test_amt_btc_checked() {
+    let amount: Amount<BTC, Checked> = amt_checked!(BTC, "0.00000001 BTC");
+    assert_eq!(format!("{}", amount), "0.00000001 BTC");
+}
+
+#[test]
+fn test_amt_btc_unchecked() {
+    let amount = amt!(BTC, "0.00000001 BTC");
+    assert_eq!(format!("{}", amount), "0.00000001 BTC");
+}
