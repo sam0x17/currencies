3b421944bd910fe06cc232a379575d55e594cef7
Sam Johnson <sam@durosoft.com>
2024-06-05 13:56:10 -0400

    update docs

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,40 @@
 //! with the ability to restrict the underlying currencies at compile-time to only allow
 //! checked arithmetic operations and requires consuming an [`Option`] in all fallible
 //! circumstances.
+//!
+//! ### Currency math
+//! ```
+//! use currencies::{*, currency::*};
+//!
+//! let apple_cost = Amount::<USD>::from_raw(3_24);
+//! let orange_cost = Amount::<USD>::from_raw(7_97);
+//! assert!(apple_cost < orange_cost);
+//! assert!(apple_cost + orange_cost > orange_cost);
+//! assert_eq!(format!("{}", apple_cost * orange_cost), "$25.82");
+//! assert_eq!(format!("{}", apple_cost * 3), "$9.72");
+//!
+//! let mut total = Amount::<DOT>::from_raw(5762244984_10000000004u128.into());
+//! total -= Amount::from_raw(1000_0000000000u128.into());
+//! total *= Amount::from_raw(2_0000000000u64.into());
+//! assert_eq!(format!("{}", total), "115244897682.0000000008 DOT");
+//!```
+//!
+//! ### Checked Math
+//! ```
+//! use currencies::{*, currency::*, safety::*};
+//!
+//! // When using currency amounts with `Safety = Checked`, the Amount struct has been specially set
+//! // up so that only checked math will be allowed, and you can still use the normal
+//! // operator-based syntax. Thus currency amounts like this should never panic and are
+//! // suitable for use in critical/infallible environments.
+//!
+//! let drink_cost = Amount::<USD, Checked>::from_raw(6_29);
+//! let movie_cost = Amount::<USD, Checked>::from_raw(24_99);
+//! let Some(outing_cost) = drink_cost + movie_cost else {
+//! 	unimplemented!("compiler forces you to handle this!")
+//! };
+//! assert_eq!(format!("{}", outing_cost), "$31.28");
+//! ```
 
 #![cfg_attr(not(feature = "std"), no_std)]
 #![warn(missing_docs)]
